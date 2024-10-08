a2e6d1054e96428e321437409837d5b46a6410bb
seongwon lee <jasmine1714@naver.com>
2024-10-08 13:24:14 +0900

    텍스트 가져오기

--- practice.py
+++ practice.py
@@ -7,6 +7,8 @@ import time
 options = Options()
 options.add_argument("--start-maximized")
 options.add_experimental_option("detach", True)
+# 자동화 메세지 옵션 제거
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
 driver = webdriver.Chrome(options=options)
 
@@ -26,13 +28,14 @@ time.sleep(3) # 사람이 입력한 것처럼 보이도록 시간 주기
 # time.sleep(2)
 
 # 2
-driver.find_element(By.ID, "query").send_keys("인공지능")
+shopping_text_element = driver.find_element(By.CLASS_NAME, "MyView-module__login_text___G0Dzv")
+shopping_text = shopping_text_element.text
+driver.find_element(By.ID, "query").send_keys(shopping_text)
 time.sleep(2)
 
 # driver.find_element(By.CSS_SELECTOR, "#search-btn").click()
 driver.find_element(By.ID, "query").send_keys(Keys.ENTER)
 time.sleep(2)
 
-driver.save_screenshot("naver_인공지능.png")
-
-driver.quit()
+driver.save_screenshot("naver_로그인.png")
+#driver.quit()
